ad82e3e9c8c87dd0d432bae77930323748dc292e
bekzatkazatov <bekzatx2x@gmail.com>
2023-03-07 22:09:08 +0600

    js2t

---
+++ script.js
@@ -0,0 +1,66 @@
+// let a = 'hello'
+// let b = 'Ivan'
+// let c =(b.replace ('I' ,'i'))
+// console.log (a.replace('h','H')+ ' '+c)         Hello ivan
+
+// let a = 'lorem ipsum dolor'
+// let b = (a.length )
+// console.log(b)                                  17
+
+// let a = 'lorem ipsum dolor sit amet'
+// let b =  a.slice (17 ,22)
+// let c = a.slice (0,6)
+// let d = a.slice (6,12)
+// let i = a.slice (12,18)
+// let j = a.slice (22,26)
+// console.log(b+c+d+i+j)                           sit lorem ipsum dolor amet
+
+// let a = 'children'
+// let b = a.toUpperCase()
+// console.log(b.replace('N','n'))                  верунть CHILDREn
+
+// let a = 'boss'
+// let b = a.slice (0,1)
+// let c = a.slice (1,2)
+// let d = a .slice (2,4)
+// console.log(b+d+c)                              верунть bsso
+
+// let a = 'sunday'
+// // let b = a.slice (0,2)
+// // let c = a.slice (4,6)
+// console.log(a.substr( 3,1))              вернуть index d
+
+// let a = 'hello'
+// let b = a.replace( 'l',"k")
+// console.log(b.replace('l','s'))          hekso
+
+// let a = 'user'
+// let b = 'new'
+// let c =  a.slice(0,2)
+// console.log(b+' '+c)                          верунть new us
+
+// let a = 'monday'
+// let b = 'day'
+// let c = (a.slice(a.indexOf('d')))
+// let d = a.slice(0,3)
+// console.log(b+c+d)                               вернуть daydaymon
+
+// let a = 'HELLO'
+// let b = a.replace('H','h')
+// let c = b.replace('L','l')
+// let d = c.replace('O',"o")
+// console.log (d)                                    вернуть 'hElLo'
+
+// let a = 5
+// let b = '400'
+// let c = a.toString()
+// let d = b.slice (1,2)
+// let f = b.slice(0,1)
+// console.log (f+c+d)                                вернуть '450'
+
+// let a = 'monkey'
+// let b = 'banana'
+// let c = a.slice(0,1)
+// let d = b.slice(1,4)
+// let i = a.slice(3,4)
+// console.log(c+d+i)                                 вернуть manak
